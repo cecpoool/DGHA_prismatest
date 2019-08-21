241d3086a55317dc4c9ca7aeea6abadbbc559168
cecpoool <cjmcook@gmail.com>
2019-08-21 13:42:32 +1000

    second

--- mmagictouchtest/index.js
+++ mmagictouchtest/index.js
@@ -27,22 +27,3 @@ const server = new GraphQLServer({
 server.start(() => console.log('Server is running on http://localhost:4000'))
 
 
-// // A `main` function so that we can use async/await
-// async function main() {
-//   // Create a new user with a new post
-//   const newUser = await prisma.createUser({
-//     username: 'Bob3',
-//     email: 'bob@prisma.io',
-//     password: "ABSOLE"
-//   })
-//   console.log(`Created new user: ${newUser.username} (ID: ${newUser.id}, password: ${newUser.password})`)
-
-//   // Read all users from the database and print them to the console
-//   const allUsers = await prisma.users()
-//   console.log(allUsers)
-
-//   const allPosts = await prisma.posts()
-//   console.log(allPosts)
-// }
-
-// main().catch(e => console.error(e))
